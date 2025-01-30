4d23a2065c55b2acbc8bc23ae64a270c1caf08a8
st4cy <eniolaao2@gmail.com>
2025-01-30 19:37:19 +0000

    initialising APIproject

---
+++ src/main/java/com/eni_cbf/APIproject/ApIprojectApplication.java
@@ -0,0 +1,13 @@
+package com.eni_cbf.APIproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eni_cbf/APIproject/ApIprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni_cbf.APIproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
